25d427c86dee1c0fdd235427c89ae2d799fa3546
Hunter Miller <svarty1@gmail.com>
2020-04-03 19:15:55 -0500

    build stuff

--- js/main.js
+++ js/main.js
@@ -84,17 +84,200 @@
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ "./resources/js/main.js":
+/*!******************************!*\
+  !*** ./resources/js/main.js ***!
+  \******************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_mobile_nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @modules/mobile-nav */ "./resources/js/modules/mobile-nav/index.js");
+/* harmony import */ var _modules_lazyload__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @modules/lazyload */ "./resources/js/modules/lazyload/index.js");
+// Import local modules
+
+
+
+/***/ }),
+
+/***/ "./resources/js/modules/lazyload/index.js":
+/*!************************************************!*\
+  !*** ./resources/js/modules/lazyload/index.js ***!
+  \************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _utilities_selectors__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @utilities/selectors */ "./resources/js/utilities/selectors/index.js");
+/* harmony import */ var _utilities_helpers__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @utilities/helpers */ "./resources/js/utilities/helpers/index.js");
+
+
+
+var Lazyload = function Lazyload() {
+  // lazyload our images
+  var images = _utilities_selectors__WEBPACK_IMPORTED_MODULE_0__["default"].wrapper.querySelectorAll('[data-lazy]');
+
+  if (Object(_utilities_helpers__WEBPACK_IMPORTED_MODULE_1__["exists"])(images)) {
+    // options
+    var options = {
+      threshold: 0.5
+    };
+
+    var preloadImage = function preloadImage(img) {
+      // find and store the image's data-lazy attribute
+      // commented out for now, but if you want to go the extra mile, then you can do all the srcset attribute stuff on the images ;)
+      // const srcset = img.dataset.srcset
+      var src = img.dataset.lazy;
+      img.src = src; // img.srcset = srcset
+      // add a class of loaded
+      // we can then use this as a hook for fade-in animations etc
+
+      img.classList.add('loaded');
+    };
+
+    var lazyLoad = new IntersectionObserver(function (entries, lazyLoad) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          preloadImage(entry.target);
+          lazyLoad.unobserve(entry.target);
+        }
+      });
+    }, options);
+    images.forEach(function (image) {
+      lazyLoad.observe(image);
+    });
+  }
+}();
+
+/* harmony default export */ __webpack_exports__["default"] = (Lazyload);
+
+/***/ }),
+
+/***/ "./resources/js/modules/mobile-nav/index.js":
+/*!**************************************************!*\
+  !*** ./resources/js/modules/mobile-nav/index.js ***!
+  \**************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _utilities_selectors__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @utilities/selectors */ "./resources/js/utilities/selectors/index.js");
+
+
+var MobileNav = function MobileNav() {
+  _utilities_selectors__WEBPACK_IMPORTED_MODULE_0__["default"].mobileNavToggle.addEventListener('click', function () {
+    this.classList.toggle('menu-toggle-active');
+    _utilities_selectors__WEBPACK_IMPORTED_MODULE_0__["default"].mobileNav.classList.toggle('menu-visible'); // set aria-expanded attribute on menu toggle button
+
+    if (this.getAttribute('aria-expanded') === 'false') {
+      this.setAttribute('aria-expanded', 'true');
+    } else {
+      this.setAttribute('aria-expanded', 'false');
+    }
+  });
+}();
+
+/* harmony default export */ __webpack_exports__["default"] = (MobileNav);
+
+/***/ }),
+
+/***/ "./resources/js/utilities/helpers/index.js":
+/*!*************************************************!*\
+  !*** ./resources/js/utilities/helpers/index.js ***!
+  \*************************************************/
+/*! exports provided: page, exists */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "page", function() { return page; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "exists", function() { return exists; });
+/* harmony import */ var _utilities_selectors__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @utilities/selectors */ "./resources/js/utilities/selectors/index.js");
+
+/**
+ * @description Test if the body id is something
+ * @param  		{string}
+ * @return 		{bool}
+ *
+ */
+
+var page = function page(name) {
+  if (!name) {
+    return _utilities_selectors__WEBPACK_IMPORTED_MODULE_0__["default"].body.getAttribute('id');
+  }
+
+  return _utilities_selectors__WEBPACK_IMPORTED_MODULE_0__["default"].body.getAttribute('id') == name;
+};
+/**
+ * @description Check if element exists the page
+ * @param  		{string} Element selector
+ * @param  		{string} Minimum number of elements
+ * @return 		{bool}
+ */
+
+
+var exists = function exists(el, limit) {
+  return el.length > 0;
+};
+
+
+
+/***/ }),
+
+/***/ "./resources/js/utilities/selectors/index.js":
+/*!***************************************************!*\
+  !*** ./resources/js/utilities/selectors/index.js ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var $$ = {
+  body: document.querySelector('body'),
+  wrapper: document.getElementById('wrapper'),
+  header: document.getElementById('header'),
+  nav: document.getElementById('nav'),
+  hero: document.getElementById('hero'),
+  main: document.getElementById('main'),
+  containerCentre: document.getElementById('container-centre'),
+  containerRight: document.getElementById('container-right'),
+  preFooter: document.getElementById('pre-footer'),
+  footer: document.getElementById('footer'),
+  mobileNav: document.getElementById('mobile-nav'),
+  mobileNavToggle: document.getElementById('mobile-nav-toggle')
+};
+/* harmony default export */ __webpack_exports__["default"] = ($$);
+
+/***/ }),
+
+/***/ "./resources/sass/main.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/main.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
 /*!***************************************************************!*\
   !*** multi ./resources/js/main.js ./resources/sass/main.scss ***!
   \***************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-!(function webpackMissingModule() { var e = new Error("Cannot find module '/mnt/c/Users/Svart/Projects/uulala/resources/js/main.js'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
-!(function webpackMissingModule() { var e = new Error("Cannot find module '/mnt/c/Users/Svart/Projects/uulala/resources/sass/main.scss'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+__webpack_require__(/*! /mnt/c/Users/Svart/Projects/uulala/resources/js/main.js */"./resources/js/main.js");
+module.exports = __webpack_require__(/*! /mnt/c/Users/Svart/Projects/uulala/resources/sass/main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
-/******/ ]);
+
+/******/ });
